f7cb970aa087de035852a970e78e0f72f0b2d2bd
aa0241 <77753344+aa0241@users.noreply.github.com>
2022-06-20 17:37:08 +0300

    Edit icons

--- Team3/App.js
+++ Team3/App.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ProfileScreen } from "./screens/ProfileScreen";
 import { HomeScreen } from "./screens/HomeScreen";
 import { CreateScreen } from "./screens/CreateScreen";
+import { Ionicons, AntDesign } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,9 +11,52 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen  name="Home" component={HomeScreen} />
-        <Tab.Screen name="Create" component={CreateScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size, style }) => (
+              <Ionicons
+                name="home"
+                color={"black"}
+                size={30}
+                style={{ paddingTop: "5%" }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Create"
+          component={CreateScreen}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons
+                name="md-add-circle-sharp"
+                size={30}
+                color={"black"}
+                style={{ paddingTop: "5%" }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign
+                name="profile"
+                size={30}
+                color="black"
+                style={{ paddingTop: "5%" }}
+              />
+            ),
+            tabBarOptions: { showLabel: false },
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
