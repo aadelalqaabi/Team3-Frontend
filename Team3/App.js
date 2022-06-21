ba97b2458b34ae5ef8468b4ddc2fd5cc6df45a00
AB <aasharq99@gmail.com>
2022-06-21 17:25:23 +0300

    ADDED User register/login

--- Team3/App.js
+++ Team3/App.js
@@ -1,63 +1,44 @@
+import { NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ProfileScreen } from "./screens/ProfileScreen";
 import { HomeScreen } from "./screens/HomeScreen";
 import { CreateScreen } from "./screens/CreateScreen";
 import { Ionicons, AntDesign } from "@expo/vector-icons";
+import Login from "./screens/authScreens/Login";
+import Register from "./screens/authScreens/Register";
+import AuthButtons from "./screens/authScreens/AuthButtons";
+import { observer } from "mobx-react";
+import authStore from "./stores/authStore";
+
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function App() {
+
+function App() {
+  const checkUser = authStore.user; 
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarShowLabel: false,
-            tabBarIcon: ({ color, size, style }) => (
-              <Ionicons
-                name="home"
-                color={"black"}
-                size={30}
-                style={{ paddingTop: "5%" }}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Create"
-          component={CreateScreen}
-          options={{
-            tabBarShowLabel: false,
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons
-                name="md-add-circle-sharp"
-                size={30}
-                color={"black"}
-                style={{ paddingTop: "5%" }}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            tabBarShowLabel: false,
-            tabBarIcon: ({ color, size }) => (
-              <AntDesign
-                name="profile"
-                size={30}
-                color="black"
-                style={{ paddingTop: "5%" }}
-              />
-            ),
-            tabBarOptions: { showLabel: false },
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        {
+        checkUser ? ( 
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarShowLabel: false, tabBarIcon: ({ color, size, style }) => (<Ionicons name="home" color={"black"} size={30} style={{ paddingTop: "5%" }}/>),}}/>
+            <Tab.Screen name="Create" component={CreateScreen} options={{ tabBarShowLabel: false, tabBarIcon: ({ color, size }) => (<Ionicons name="md-add-circle-sharp" size={30} color={"black"} style={{ paddingTop: "5%" }}/>),}}/>
+            <Tab.Screen name="Profile" component={ProfileScreen} options={{ tabBarShowLabel: false, tabBarIcon: ({ color, size }) => (<AntDesign name="profile" size={30} color="black" style={{ paddingTop: "5%" }}/>), tabBarOptions: { showLabel: false },}}/>
+          </Tab.Navigator>
+        ) : ( 
+          <Stack.Navigator>
+              <Stack.Screen name="Set Up Account" component={AuthButtons} />
+              <Stack.Screen name="Register" component={Register} />
+              <Stack.Screen name="Login" component={Login} />
+          </Stack.Navigator>
+        )}
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
+
+export default observer(App);
--- Team3/screens/ProfileScreen.js
+++ Team3/screens/ProfileScreen.js
@@ -1,8 +1,10 @@
 import { Text, View } from "react-native";
+import Logout from "./authScreens/Logout";
 export function ProfileScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>profile!</Text>
+      <Logout/>
     </View>
   );
 }
